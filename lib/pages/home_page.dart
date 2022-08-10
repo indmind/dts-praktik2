ef3c5eea343633a99eeef93b95e66fafd6a39aa9
indmind <sattanationmail@gmail.com>
2022-08-10 21:11:26 +0700

    listview sederhana

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,104 +8,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _angka1 = TextEditingController();
-  final TextEditingController _angka2 = TextEditingController();
-
-  double _result = 0;
-
-  @override
-  void dispose() {
-    _angka1.dispose();
-    _angka2.dispose();
-    super.dispose();
-  }
+  final List<String> countries = [
+    'Indonesia',
+    'Malaysia',
+    'Singapore',
+    'Italia',
+    'Inggris',
+    'Belanda',
+    'Argentina',
+    'Chile',
+    'Mesir',
+    'Uganda',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Masukkan 2 Angka'),
+        title: const Text('ListView sederhana'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Text(
-              "Masukkan Dua Angka".toUpperCase(),
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            TextField(
-              controller: _angka1,
-              decoration: const InputDecoration(
-                labelText: 'Masukkan Angka Pertama',
-              ),
-            ),
-            TextField(
-              controller: _angka2,
-              decoration: const InputDecoration(
-                labelText: 'Masukkan Angka Kedua',
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _result = double.parse(_angka1.text) +
-                          double.parse(_angka2.text);
-                    });
-                  },
-                  child: const Text('+'),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => const Divider(height: 0),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(countries[index]),
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Memilih ${countries[index]}'),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _result = double.parse(_angka1.text) -
-                          double.parse(_angka2.text);
-                    });
-                  },
-                  child: const Text('-'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _result = double.parse(_angka1.text) *
-                          double.parse(_angka2.text);
-                    });
-                  },
-                  child: const Text('*'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _result = double.parse(_angka1.text) /
-                          double.parse(_angka2.text);
-                    });
-                  },
-                  child: const Text('/'),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _result = 0;
-                    _angka1.clear();
-                    _angka2.clear();
-                  });
-                },
-                child: const Text("Bersihkan"),
-              ),
-            ),
-            Text(
-              'Hasilnya adalah $_result',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ],
-        ),
+              );
+            },
+          );
+        },
+        itemCount: countries.length,
       ),
     );
   }
